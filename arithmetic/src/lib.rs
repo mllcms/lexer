663acd344773eaad27b583a871a11f8ae85c451d
Mll <mllcms@163.com>
2024-03-16 19:07:26 +0800

    perf: 优化解析

--- arithmetic/src/lib.rs
+++ arithmetic/src/lib.rs
@@ -48,66 +48,52 @@ impl Symbol {
 /// 计算算式表达式
 pub fn arithmetic(expr: &str) -> Result<f64, String> {
     let mut symbol = Vec::new();
-    let mut number: Vec<f64> = Vec::new();
-    let mut prev = char::default();
-    let mut buf = String::new();
+    let mut number = Vec::new();
+    let mut buffer = String::new();
+    let mut before = char::default();
 
     for c in expr.chars() {
-        // println!("{:?}", symbol);
-        // println!("{:?}", number);
-        // println!("{:?}\n", buf);
-
         match c {
-            '0'..='9' | '.' => buf.push(c),
+            '0'..='9' | '.' => buffer.push(c),
             _ if c.is_ascii_whitespace() => continue,
             _ => {
-                let s = Symbol::parse(c)?;
-
-                // 遇到 +- 如果上一个字符不是 数字, ’)‘, '%' 就认定为正负数
-                if matches!(s, Symbol::Add | Symbol::Sub if !matches!(prev, '0'..='9' | ')' | '%')) {
-                    buf.push(c);
-                    continue;
-                }
-
                 // 解析为浮点数并重置缓冲区
-                if !buf.is_empty() {
-                    number.push(parse_f64(&buf)?);
-                    buf.truncate(0);
+                if !buffer.is_empty() {
+                    number.push(parse_f64(&buffer)?);
+                    buffer.truncate(0);
                 }
 
-                match s {
-                    // 括号前面为数字省略 *
-                    Symbol::ParL if prev.is_ascii_digit() => {
-                        symbol.push(Symbol::Mul);
-                        symbol.push(Symbol::ParL);
-                    }
-                    Symbol::ParL => symbol.push(s),
-                    // 遇到 ’)‘ 开始回栈直到遇见 ‘(’
-                    Symbol::ParR => back_stack(&mut symbol, &mut number, Some(Symbol::ParL))?,
-                    // 计算百分号
-                    Symbol::Per => s.compute(&mut number)?,
-                    _ => match symbol.last_mut() {
+                match Symbol::parse(c)? {
+                    s @ Symbol::Per => s.compute(&mut number)?,                     // 计算百分号
+                    s @ Symbol::ParL if !before.is_ascii_digit() => symbol.push(s), // 左边不是数字的 '(' 不需要处理
+                    Symbol::Add | Symbol::Sub if !matches!(before, '0'..='9' | ')' | '%') => buffer.push(c), // 判定为正负数
+                    Symbol::ParR => back_stack(&mut symbol, &mut number, Some(Symbol::ParL))?, // 遇到 ’)‘ 开始回栈直到遇见 ‘(’
+                    s => {
                         // 先乘除
-                        Some(last @ (Symbol::Mul | Symbol::Div)) => {
+                        if let Some(last @ (Symbol::Mul | Symbol::Div)) = symbol.last() {
                             last.compute(&mut number)?;
-                            *last = s
+                            symbol.pop();
                         }
-                        _ => symbol.push(s),
-                    },
+                        // ’(‘ 左边是数字补上 ‘*’ 号
+                        if matches!(s, Symbol::ParL) {
+                            symbol.push(Symbol::Mul)
+                        }
+                        symbol.push(s)
+                    }
                 }
             }
         }
 
+        // println!("symbol: {:?}", symbol);
+        // println!("number: {:?}", number);
+        // println!("buffer: {:?}\n", buffer);
+
         // 记录上一个字符用于后续匹配
-        prev = c;
+        before = c;
     }
 
-    // println!("{:?}", symbol);
-    // println!("{:?}", number);
-    // println!("{:?}", buf);
-
-    if !buf.is_empty() {
-        number.push(parse_f64(&buf)?)
+    if !buffer.is_empty() {
+        number.push(parse_f64(&buffer)?)
     }
     back_stack(&mut symbol, &mut number, None)?;
 
@@ -134,7 +120,9 @@ fn back_stack(symbol: &mut Vec<Symbol>, number: &mut Vec<f64>, meet: Option<Symb
 }
 
 #[test]
-fn individually_tested() {}
+fn individually_tested() {
+    assert_eq!(Ok(90.0), arithmetic("60/2(1+2)"));
+}
 
 #[test]
 fn test_simple_arithmetic() {
@@ -159,6 +147,9 @@ fn test_simple_arithmetic() {
     assert_eq!(Ok(9.5), arithmetic("10 - 50%"));
     assert_eq!(Ok(50.0), arithmetic("100 * 50%"));
     assert_eq!(Ok(50.0), arithmetic("25 / 50%"));
+
+    assert_eq!(Ok(3.0), arithmetic("1.25 + 1.75"));
+    assert_eq!(Ok(125.0), arithmetic("1.25 * 100"));
 
     assert_eq!(Ok(0.3333333333333333), arithmetic("1 / 3"));
     assert_eq!(Ok(3.142857142857143), arithmetic("22 / 7"));
